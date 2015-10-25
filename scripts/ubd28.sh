8d534c4630cd60a1c87fa3e586442e2600469c0c
Adrian Gschwend <ktk@netlabs.org>
2015-10-26 00:10:30 +0100

    Initial import

---
+++ scripts/ubd28.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm output/ubd28.nt
+java -jar ~/workspace.p3/RMLProcessor/target/RMLMapper-0.1.jar -M config/ubd28.ttl -O output/ubd28.nt
+perl -pi.back -e 's/(\/canton\/)([A-Z]{2})/\1\L\2/' output/ubd28.nt

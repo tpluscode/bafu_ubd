ea3e0f5ac93e14535714f2cdde3df65b2691c0c4
Reto Gmür <reto.gmuer@zazuko.com>
2015-10-27 14:43:02 +0100

    creating utf-8 to new dir

--- scripts/utf8.sh
+++ scripts/utf8.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
-for file in ../input/ubd/28/*.csv
+#!/bin/bash
+if [ ! -d input ]; then 
+	echo "script muss be run from project root"
+	exit 1
+fi
+mkdir -p target/input-utf8 
+for file in input/ubd/28/*.csv
 do
-        iconv -f windows-1252 -t utf8 "$file" > "$file.new"  &&
-        mv -f "$file.new" "$file"
+	TARGETFILE="target/input-utf8/$file";
+	echo $TARGETFILE;
+	mkdir -p $(dirname $TARGETFILE);
+        iconv -f windows-1252 -t utf8 "$file" > $TARGETFILE  
 done

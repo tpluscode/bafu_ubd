ff0dde2ddd52336cff01a7d64ca1a3191e04f33c
Adrian Gschwend <ktk@netlabs.org>
2015-10-27 14:52:05 +0100

    Removed output dir

--- scripts/ubd28.sh
+++ scripts/ubd28.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm output/ubd28.nt
-java -jar ./lib/RMLMapper-0.1.jar -M config/ubd28.ttl -O output/ubd28.nt
-perl -pi.back -e 's/(\/canton\/)([A-Z]{2})/\1\L\2/' output/ubd28.nt
+rm target/ubd28.nt
+java -jar ./lib/RMLMapper-0.1.jar -M config/ubd28.ttl -O target/ubd28.nt
+perl -pi.back -e 's/(\/canton\/)([A-Z]{2})/\1\L\2/' target/ubd28.nt

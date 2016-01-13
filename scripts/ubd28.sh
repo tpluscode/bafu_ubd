c132c12cc495a4fd0e6c5d051766ba3c1a1c2413
Adrian Gschwend <ktk@netlabs.org>
2016-01-13 11:44:30 +0100

    Adjusted serdi options, seems to have changed

--- scripts/ubd28.sh
+++ scripts/ubd28.sh
@@ -2,4 +2,4 @@
 rm target/ubd28.nt
 java -Djava.util.logging.config.file=config/log-config -jar ./lib/RMLMapper-0.1.jar -M config/ubd28.ttl -O target/ubd28.nt 2>&1 | grep -v DEBUG
 perl -pi.back -e 's/(\/canton\/)([A-Z]{2})/\1\L\2/' target/ubd28.nt
-serdi -b -f -i turtle -o ntriples input/meta/qb.ttl >> target/ubd28.nt
+serdi -i turtle -o ntriples input/meta/ubd28/qb.ttl >> target/ubd28.nt

6e93a2cd0ca6496dcd752a66a089bf3f83e3a134
Adrian Gschwend <ktk@netlabs.org>
2016-02-24 16:05:34 +0100

    Included Cube definitions for UBD66

--- scripts/ubd66.sh
+++ scripts/ubd66.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm target/ubd66.nt
 java -Djava.util.logging.config.file=config/log-config -jar ./lib/RMLMapper-0.1.jar -M config/ubd66.ttl -O target/ubd66.nt 2>&1 | grep -v DEBUG
-#serdi -i turtle -o ntriples input/meta/ubd66/qb.ttl >> target/ubd66.nt
+serdi -i turtle -o ntriples input/meta/ubd66/qb.ttl >> target/ubd66.nt

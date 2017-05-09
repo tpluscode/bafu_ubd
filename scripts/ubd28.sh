fc961f505beb87da989ae2fee3b9d1b9535f03ca
Adrian Gschwend <ktk@netlabs.org>
2017-05-09 14:56:15 +0200

    Cleanup

--- scripts/ubd28.sh
+++ scripts/ubd28.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm target/ubd28.nt
-#java -Djava.util.logging.config.file=config/log-config -jar ./lib/RMLMapper-0.1.jar -M config/ubd28.ttl -O target/ubd28.nt 2>&1 | grep -v DEBUG
-java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/ubd28.ttl -o target/ubd28.nt | grep -v DEBUG
+java -Djava.util.logging.config.file=config/log-config -jar ./lib/RMLMapper-0.1.jar -M config/ubd28.ttl -O target/ubd28.nt 2>&1 | grep -v DEBUG
+#java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/ubd28.ttl -o target/ubd28.nt | grep -v DEBUG
 perl -pi.back -e 's/(\/canton\/)([A-Z]{2})/\1\L\2/' target/ubd28.nt
 serdi -i turtle -o ntriples -p ubd28 input/meta/ubd28/qb.ttl >> target/ubd28.nt

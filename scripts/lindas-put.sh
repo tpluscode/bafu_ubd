ec9fc2ea8b73d1d60f988d698534cbd57ec2fdaa
Adrian Gschwend <ktk@netlabs.org>
2017-07-26 20:43:23 +0200

    Fixed UBD66 mapping for siteplot

--- scripts/lindas-put.sh
+++ scripts/lindas-put.sh
@@ -3,11 +3,11 @@ curl -n \
      -X PUT \
      -H Content-Type:application/n-triples \
      -T target/ubd28.nt \
-     -G https://lindasprd.netrics.ch:8443/lindas \
+     -G https://lindas-data.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/FOEN/UBD28
 curl -n \
      -X PUT \
      -H Content-Type:application/n-triples \
      -T target/ubd66.nt \
-     -G https://lindasprd.netrics.ch:8443/lindas \
+     -G https://lindas-data.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/FOEN/UBD66
